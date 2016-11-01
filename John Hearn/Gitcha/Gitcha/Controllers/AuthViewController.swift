437dd4c2c3f960dceb818aab8dba59166d42c867
John Hearn <jhearn@gmail.com>
2016-11-01 09:33:40 -0700

    initial commit

---
+++ John Hearn/Gitcha/Gitcha/Controllers/AuthViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AuthViewController.swift
+//  Gitcha
+//
+//  Created by John D Hearn on 11/1/16.
+//  Copyright © 2016 Bastardized Productions. All rights reserved.
+//
+
+import UIKit
+
+class AuthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ John Hearn/Gitcha/Gitcha/HomeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  HomeViewController.swift
+//  Gitcha
+//
+//  Created by John D Hearn on 11/1/16.
+//  Copyright © 2016 Bastardized Productions. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
